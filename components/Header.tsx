6a379775637efd2092aaa143280f39c07f0a0847
Hyunchul Kim <mail@hyunchu.kim>
2024-11-29 12:14:07 +0900

    update head & footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,46 +5,35 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from 'next/image'
 
 const Header = () => {
-  let headerClass = 'flex items-center w-full bg-white dark:bg-gray-950 justify-between py-10'
+  let headerClass = "w-full fixed top-0 left-0"
   if (siteMetadata.stickyNav) {
-    headerClass += ' sticky top-0 z-50'
+    headerClass += ' sticky top-0'
   }
 
   return (
     <header className={headerClass}>
-      {/* <Link href="/" aria-label={siteMetadata.headerTitle}>
-        <div className="flex items-center justify-between">
-          <div className="mr-3">
-            <Logo />
-          </div>
-          {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
-          ) : (
-            siteMetadata.headerTitle
-          )}
-        </div>
-      </Link> */}
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96">
+      <div className="flex justify-center p-4">
+      <Image src="static/images/logo_ver_2.svg" width={119.62} height={73.71}alt='Crane logo'></Image>
+      </div>
+
+      <div className="flex justify-between items-center py-4 px-16 font-bold">
+        {/* <div className="no-scrollbar hidden max-w-40 items-center space-x-4 overflow-x-auto sm:flex sm:space-x-6 md:max-w-72 lg:max-w-96"> */}
           {headerNavLinks
             .filter((link) => link.href !== '/')
             .map((link) => (
               <Link
                 key={link.title}
                 href={link.href}
-                className="block font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+                className="block text-gray-900 hover:text-black"
               >
                 {link.title}
               </Link>
             ))}
-        </div>
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
+        {/* </div> */}
+        {/* <MobileNav /> */}
       </div>
     </header>
   )
